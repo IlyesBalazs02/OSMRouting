9023ddafc8950f915298654377f89fb34d7c2d44
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-06-20 00:23:53 +0200

    Way class created

---
+++ OSMRouting/Way.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSMRouting
+{
+	public class Way
+	{
+		public long Id { get; set; }
+		public List<Node> Nodes { get; set; }
+	}
+}
