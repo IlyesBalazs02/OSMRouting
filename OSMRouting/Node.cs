6ec64fb8b61ce732a796f0913db36b94f256ee77
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-06-20 00:54:58 +0200

    JsonPropertyName attributes added

--- OSMRouting/Node.cs
+++ OSMRouting/Node.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace OSMRouting
 {
     public class Node
     {
+        [JsonPropertyName("id")]
         public long Id { get; set; }
-        public double lat {  get; set; }
-        public double lon { get; set; }
+
+		[JsonPropertyName("lat")]
+		public double lat {  get; set; }
+
+		[JsonPropertyName("lon")]
+		public double lon { get; set; }
     }
 }
--- OSMRouting/Way.cs
+++ OSMRouting/Way.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace OSMRouting
 {
 	public class Way
 	{
+		[JsonPropertyName("id")]
 		public long Id { get; set; }
 		public List<Node> Nodes { get; set; }
 	}
