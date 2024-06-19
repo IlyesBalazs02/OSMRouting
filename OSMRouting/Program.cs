938d9579a6ee1739f0128ee144817c83ace98af3
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-06-20 00:55:19 +0200

    test input added

--- OSMRouting/Program.cs
+++ OSMRouting/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OSMRouting
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			double maxLat = 47.71424;
+			double maxLon = 15.80773;
+			double minLat = 47.70812;
+			double minLon = 15.80381;
+
+			var a = new OsmHttpRequest(minLat, minLon, maxLat, maxLon);
+		}
+	}
+}
+
+
+
