9e781dddf62de01fa2c432d9f30484bc92a9842b
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-06-21 10:08:51 +0200

    Graphnode class created

---
+++ OSMRouting/GraphNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSMRouting
+{
+	public class GraphNode
+	{
+		public Node nodePointer { get; private set; }
+
+		public Dictionary<Node, double> Neighbours { get; set; }
+
+        public GraphNode(Node nodePointer)
+        {
+            this.nodePointer = nodePointer;
+            Neighbours = new Dictionary<Node, double>();
+        }
+    }
+}
